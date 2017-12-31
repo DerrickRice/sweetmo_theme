e4ebcbcb323d7473de3f68639f8d77d8f68022aa
Derrick Rice <derrick.rice@gmail.com>
2017-12-30 20:34:29 -0500

    rebrand

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
  * before the init hook. The init hook is too late for some features, such as indicating
  * support post thumbnails.
  */
-function taste_theme_setup() {
+function sweetmo_theme_setup() {
 
 	remove_action( 'omega_before_header', 'omega_get_primary_menu' );	
 	add_action( 'omega_header', 'omega_get_primary_menu' );
@@ -20,21 +20,21 @@ function taste_theme_setup() {
 			'default-image' => get_stylesheet_directory_uri() . '/images/header.jpg'
 			));
 
-	add_action( 'omega_after_header', 'taste_intro' );
+	add_action( 'omega_after_header', 'sweetmo_intro' );
 
-	add_filter( 'omega_site_description', 'taste_site_description' );
+	add_filter( 'omega_site_description', 'sweetmo_site_description' );
 
-	add_action( 'wp_enqueue_scripts', 'taste_scripts_styles' );
+	add_action( 'wp_enqueue_scripts', 'sweetmo_scripts_styles' );
 
-	load_child_theme_textdomain( 'taste', get_stylesheet_directory() . '/languages' );
+	load_child_theme_textdomain( 'sweetmo', get_stylesheet_directory() . '/languages' );
 }
 
-add_action( 'after_setup_theme', 'taste_theme_setup', 11 );
+add_action( 'after_setup_theme', 'sweetmo_theme_setup', 11 );
 
 /**
  * Loads the intro.php template.
  */
-function taste_intro() {
+function sweetmo_intro() {
 	if (get_header_image()) {
 		echo '<div class="site-intro"><img class="header-image" src="' . esc_url( get_header_image() ) . '" alt="' . esc_html(get_bloginfo( 'description' )) . '" /></div>';
 	}
@@ -43,7 +43,7 @@ function taste_intro() {
 /**
  * disable site description
  */
-function taste_site_description($desc) {
+function sweetmo_site_description($desc) {
 	$desc = "";
 	return $desc;
 }
@@ -51,29 +51,29 @@ function taste_site_description($desc) {
 /**
  * Enqueue Scripts and Google fonts
  */
-function taste_scripts_styles() {
- 	wp_enqueue_style('taste-fonts', taste_fonts_url(), array(), null );
+function sweetmo_scripts_styles() {
+ 	wp_enqueue_style('sweetmo-fonts', sweetmo_fonts_url(), array(), null );
  	wp_enqueue_script('jquery-superfish', get_stylesheet_directory_uri() . '/js/menu.js', array('jquery'), '1.0.0', true );
- 	wp_enqueue_script('taste-init', get_stylesheet_directory_uri() . '/js/init.js', array('jquery'));
+ 	wp_enqueue_script('sweetmo-init', get_stylesheet_directory_uri() . '/js/init.js', array('jquery'));
 }
 
 /**
  * Register custom fonts.
  */
-function taste_fonts_url() {
+function sweetmo_fonts_url() {
 	$fonts_url = '';
 	 
 	/* Translators: If there are characters in your language that are not
 	* supported by Lora, translate this to 'off'. Do not translate
 	* into your own language.
 	*/
-	$satisfy = _x( 'on', 'Satisfy font: on or off', 'taste' );
+	$satisfy = _x( 'on', 'Satisfy font: on or off', 'sweetmo' );
 	 
 	/* Translators: If there are characters in your language that are not
 	* supported by Open Sans, translate this to 'off'. Do not translate
 	* into your own language.
 	*/
-	$open_sans = _x( 'on', 'Open Sans font: on or off', 'taste' );
+	$open_sans = _x( 'on', 'Open Sans font: on or off', 'sweetmo' );
 	 
 	if ( 'off' !== $satisfy || 'off' !== $open_sans ) {
 		$font_families = array();
