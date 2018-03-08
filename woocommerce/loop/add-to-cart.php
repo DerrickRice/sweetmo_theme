5c64525d6ac9b1a699c0be92e5dfd99f3dffa6b1
Derrick Rice <derrick.rice@gmail.com>
2018-03-08 11:03:53 -0500

    Fixing WooCommerce for pass sales

---
+++ woocommerce/loop/add-to-cart.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Loop Add to Cart
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/loop/add-to-cart.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     3.3.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+global $product;
+
+$category_id = get_cat_ID('pass');
+if ($category_id != 0 && in_array($category_id, $product->get_category_ids())) {
+	/* For passes, require that folks look into the options. */
+	echo apply_filters( 'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
+		sprintf( '<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
+			// permalink only, rather than add_to_cart_url
+			esc_url( $product->get_permalink() ),
+			esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
+			esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
+			isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
+			// hard coding this
+			esc_html(__( 'Select options', 'woocommerce' ))
+		),
+	$product, $args );
+}
+else {
+	/* What WooCommerce normally does */
+	echo apply_filters( 'woocommerce_loop_add_to_cart_link', // WPCS: XSS ok.
+		sprintf( '<a href="%s" data-quantity="%s" class="%s" %s>%s</a>',
+			esc_url( $product->add_to_cart_url() ),
+			esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
+			esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
+			isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
+			esc_html( $product->add_to_cart_text() )
+		),
+	$product, $args );
+}
