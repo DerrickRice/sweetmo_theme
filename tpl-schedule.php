95c28990ceff2630304a3efd3a80a0a78853d7ab
Derrick Rice <derrick.rice@gmail.com>
2018-03-15 13:59:36 -0400

    Different approach to schedule page

--- tpl-schedule.php
+++ tpl-schedule.php
@@ -6,18 +6,23 @@
 
 remove_action( 'omega_after_main', 'omega_primary_sidebar' );
 
+function tpl_schedule_content_replacement($ign) {
+	return get_template_part('php_pages/schedule');
+}
+add_filter( 'the_content', 'tpl_schedule_content_replacement', 99);
+
 get_header(); ?>
 
 	<main  class="<?php echo omega_apply_atomic( 'main_class', 'content' );?>" <?php omega_attr( 'content' ); ?>>
 
 		<?php
 		do_action( 'omega_before_content' );
-		?>
-		<b>DERRICK WAS HERE</b>
-		<?php
-//		do_action( 'omega_content' );
+
+		do_action( 'omega_content' );
+
 		do_action( 'omega_after_content' );
 		?>
+
 	</main><!-- .content -->
 
 <?php get_footer(); ?>
